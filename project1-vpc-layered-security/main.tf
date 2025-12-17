7d428b0b5e0f2b5acea84e244ce882db811dd0ff
New-Cloud-Leaner <sidd_ghosh@myyahoo.com>
2025-12-17 07:23:55 +0530

    fixed the error for private subnet resource block

--- project1-vpc-layered-security/main.tf
+++ project1-vpc-layered-security/main.tf
@@ -62,7 +62,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.private_subnet_name
+  cidr_block              = var.private_subnet_cidr
   availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = false
   tags = {
